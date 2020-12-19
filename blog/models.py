d8894be68123220315b4cc33db28f8e515772770
pavel <paul601@mail.ru>
2020-12-19 15:08:18 +0500

    Try to add feych_with_comments_count

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,43 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models import Count
+
+
+class PostQuerySet(models.QuerySet):
+
+    def year(self, year):
+        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
+        return posts_at_year
+
+
+    def popular(self):
+        popular_posts = self.annotate(likes_count=Count('likes'))\
+                            .order_by('-likes_count')
+        return popular_posts
+
+
+    def fetch_with_comments_count(self):
+
+        most_popular_posts = self.popular()
+
+        most_popular_posts_ids = [post.id for post in most_popular_posts]
+
+        posts_with_comments = self.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('post_commented'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+
+        for post in most_popular_posts:
+            post.comments_count = count_for_id[post.id]
+
+        return
+
+
+class TagQuerySet(models.QuerySet):
+
+    def popular(self):
+        popular_tags = self.annotate(tags_count=Count('posts__tags')).order_by('-tags_count')
+        return popular_tags
 
 
 class Post(models.Model):
@@ -13,6 +50,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", limit_choices_to={'is_staff': True})
     likes = models.ManyToManyField(User, related_name="liked_posts", verbose_name="Кто лайкнул", blank=True)
     tags = models.ManyToManyField("Tag", related_name="posts", verbose_name="Теги")
+
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -28,6 +67,7 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField("Тег", max_length=20, unique=True)
+    objects = TagQuerySet.as_manager()
 
     def __str__(self):
         return self.title
@@ -58,3 +98,5 @@ class Comment(models.Model):
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
 
-def serialize_post1(post):
+def serialize_post(post):
 
     return {
         "title": post.title,
@@ -18,14 +18,13 @@ def serialize_post1(post):
     }
 
 
-def serialize_post(post):
+def serialize_post_optimized(post):
 
     return {
         "title": post.title,
         "teaser_text": post.text[:200],
         "author": post.author.username,
         "comments_amount": post.comments_count,
-        # post.post_commented__count
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
@@ -42,26 +41,16 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count').prefetch_related('author')[:5:]
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
 
-    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('post_commented'))
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
+    most_popular_tags = Tag.objects.popular()[:5]
 
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_fresh_posts = Post.objects.order_by('-published_at').prefetch_related('author')[:5]
 
-
-    fresh_posts = Post.objects.annotate(comments_count=Count('post_commented')).order_by('published_at').prefetch_related('author')
-    most_fresh_posts = list(fresh_posts)[-5:]
-
-    tags = Tag.objects.annotate(tags_count=Count('posts__tags')).order_by('-tags_count')
-    most_popular_tags = tags[:5:]
 
     context = {
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
